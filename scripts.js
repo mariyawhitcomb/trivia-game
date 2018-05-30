b0c1c0c5d9659743621de90805f06aaac3b88ec0
Mariya Whitcomb <mariyawhitcomb@Mariyas-MacBook-Pro.local>
2018-05-30 16:23:12 -0400

    fixed checkAnswer

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 var button = document.querySelector('button')
-// button.addEventListener('click', checkAnswer)
+button.addEventListener('click', checkAnswer)
 var answerValue
 var score = 0
 let i = 0
 function currentQuestion (i) {
-    generateQuestion(i)
+  generateQuestion(i)
 }
 var questions = [
   {
@@ -28,28 +28,27 @@ function generateQuestion (i) {
 
 currentQuestion(i)
 
-
-// function checkAnswer (radio) {
-//   radio.preventDefault()
-//   getAnswerValue()
-//   if (answerValue === 'correct') {
-//     console.log('correct')
-//     score +=10
-//     console.log(score)
-//     document.body.classList.add('good-job')
-//   } else {
-//       console.log('wrong')
-//       document.body.classList.add('game-over')
-//     }
-// }
-// function getAnswerValue () {
-//   if (document.getElementById('answer1').checked) {
-//     answerValue = document.getElementById('answer1').value
-//   }
-//   if (document.getElementById('answer2').checked) {
-//     answerValue = document.getElementById('answer2').value
-//   }
-//   if (document.getElementById('answer3').checked) {
-//     answerValue = document.getElementById('answer3').value
-//   }
-// }
+function checkAnswer (radio) {
+  radio.preventDefault()
+  getAnswerValue()
+  if (answerValue === questions[i].answer) {
+    console.log('correct')
+    score +=10
+    console.log(score)
+    document.body.classList.add('good-job')
+  } else {
+      console.log('wrong')
+      document.body.classList.add('game-over')
+    }
+}
+function getAnswerValue () {
+  if (document.getElementById('answer1').checked) {
+    answerValue = document.getElementById('choice1').innerText
+  }
+  if (document.getElementById('answer2').checked) {
+    answerValue = document.getElementById('choice2').innerText
+  }
+  if (document.getElementById('answer3').checked) {
+    answerValue = document.getElementById('choice3').innerText
+  }
+}
