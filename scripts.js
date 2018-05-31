135f74ef6cc33dba039a38c8a105332b17260fd7
Mariya Whitcomb <mariyawhitcomb@Mariyas-MacBook-Pro.local>
2018-05-31 15:34:12 -0400

    created uncheck funciton

--- scripts.js
+++ scripts.js
@@ -56,7 +56,7 @@ var questions = [
     answer: 'answer1'
   }
 ]
-
+var radioButton = document.querySelectorAll('.radio-button')
 var newGame = document.querySelector('.button-end')
 newGame.addEventListener('click', restart)
 var startButton = document.querySelector('.button-start')
@@ -64,10 +64,8 @@ startButton.addEventListener('click', begin)
 var button = document.querySelector('#submit')
 button.addEventListener('click', checkAnswer)
 var answerValue
-var buttonNextCorrect = document.querySelector('.button-next-correct')
-buttonNextCorrect.addEventListener('click', changeQ)
-var buttonNextIncorrect = document.querySelector('.button-next-incorrect')
-buttonNextIncorrect.addEventListener('click', changeQ)
+var buttonNext = document.querySelectorAll('.button-next')
+buttonNext.forEach(element => element.addEventListener('click', changeQ))
 var displayQuestions = document.querySelector('.question')
 var score = 0
 var highScore = window.localStorage
@@ -80,7 +78,13 @@ function refill () {
   }
   return index
 }
-
+function uncheck () {
+  for (let r = 0; r < radioButton.length; r++) {
+    if (radioButton[r].checked) {
+      radioButton[r].checked = false
+    }
+  }
+}
 function randomQuestion () {
   i = index[Math.floor(Math.random() * index.length)]
   var num = index.indexOf(i)
@@ -94,7 +98,7 @@ function generateQuestion () {
   for (let x = 0; x < questions[i].prompts.length; x++) {
     document.querySelector(`#choice${x + 1}`).innerText = questions[i].prompts[x]
   }
-  document.querySelector('.question-number').innerText = `Question ${questions.length-index.length} of ${questions.length}`
+  document.querySelector('.question-number').innerText = `Question ${questions.length - index.length} of ${questions.length}`
 }
 
 function changeQ () {
@@ -103,11 +107,11 @@ function changeQ () {
     console.log(i)
     document.body.classList.remove('good-job')
     document.body.classList.remove('game-over')
+    uncheck()
   } else {
     getHighScore()
     document.body.classList.add('game-end')
     document.querySelector('.score-display').innerText = `Your score is ${score}\nHigh score is ${parseFloat(highScore.getItem('highScore'))}`
-
   }
 }
 function checkAnswer (radio) {
@@ -152,10 +156,10 @@ function startGame () {
 startGame()
 function getHighScore () {
   if (score > highScore.getItem('highScore')) {
-      highScore.setItem('highScore', `${score}`)
-      alert(`Congratulations!\nNew High Score is ${score}`)
+    highScore.setItem('highScore', `${score}`)
+    alert(`Congratulations!\nNew High Score is ${score}`)
   }
 }
-function resetScore(){
-    highScore.setItem('highScore', `0`)
+function resetScore () {
+  highScore.setItem('highScore', `0`)
 }
